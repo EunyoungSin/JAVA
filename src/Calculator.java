79c54700b40353ec900172939951888cbed36761
EunyoungSin <ey5742@gmail.com>
2023-09-12 19:01:29 +0900

    0912

---
+++ src/Calculator.java
@@ -0,0 +1,13 @@
+// 클래스 단위로 코딩
+// 시작, 메인 클래스
+public class Calculator {
+    // main(){  } 메서드
+    public static void main(String[] args) {
+        int a, b, sum;
+        a = 1;
+        b = 1;
+        sum = a+b;
+        // 출력 : sout
+        System.out.println(sum);
+    }
+}
---
+++ src/Operator.java
@@ -0,0 +1,11 @@
+public class Operator {
+    public static void main(String[] args) {
+        int kor, eng; // 변수선언->기억공간이 만들어진다.
+            kor = 77;
+            eng = 87;
+        int tot;
+        tot = kor + eng;
+        System.out.println("총점:"+tot);
+        System.out.println("평균:"+(tot/2));
+    }
+}
