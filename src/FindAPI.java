ab3a410e03b236a391df4d16ecaf3772ee9f287e
EunyoungSin <ey5742@gmail.com>
2023-09-16 01:11:30 +0900

    0912

---
+++ src/FindAPI.java
@@ -0,0 +1,13 @@
+public class FindAPI {
+    public static void main(String[] args) {
+        int a;
+        // AAA b;
+        a=10;
+        System.out.println("a = " + a);
+        // java>lang 안의 클래스는 자주 사용하는 클래스라 위치 생략가능
+        java.lang.String s;
+        s="APPLE";
+        System.out.println("s = "+ s);
+        System.out.println(s.length());
+    }
+}
