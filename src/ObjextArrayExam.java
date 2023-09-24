944f948c3b61fc1e3c460de187092ccbfd77e882
EunyoungSin <ey5742@gmail.com>
2023-09-25 00:25:05 +0900

    0924

---
+++ src/ObjextArrayExam.java
@@ -0,0 +1,27 @@
+public class ObjextArrayExam {
+    public static void main(String[] args) {
+        Movie[] m=new Movie[3];
+        m[0]=new Movie();
+        m[0].mtitle="A영화";
+        m[0].mmajor="박매일";
+        m[0].mtime=160;
+        m[0].mlevel=15;
+        m[0].mday="2023-01-15";
+
+        m[1]=new Movie();
+        m[1].mtitle="B영화";
+        m[1].mmajor="박매일";
+        m[1].mtime=160;
+        m[1].mlevel=15;
+        m[1].mday="2023-01-15";
+
+        m[2]=new Movie();
+        m[2].mtitle="A영화";
+        m[2].mmajor="박매일";
+        m[2].mtime=160;
+        m[2].mlevel=15;
+        m[2].mday="2023-01-15";
+
+        
+    }
+}
